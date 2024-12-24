d038d691d2239771bb9b2981ea965b1dbd8c220f
piyush <piyushyadav832988@gmail.com>
2024-12-24 20:08:54 +0530

    changes in sumOFNatural

--- Recursionn/sumOfNatural.cpp
+++ Recursionn/sumOfNatural.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
 using namespace std;
 
-// int sumOfNatural(int n){
-//     if(n==0){
-//         return 0;
-//     }
+int sumOfNatural(int n){
+    if(n==0){
+        return 0;
+    }
     
-//     else{
-//         return sumOfNatural(n-1)+n;
-//     }
+    else{
+        return sumOfNatural(n-1)+n;
+    }
     
-// }
-
-// int main(){
-//     int x = sumOfNatural(5);
-//     cout<<x;
-// }
-
-
-int SumOFNatural(int n){
-    return n*(n+1)/2;
 }
 
 int main(){
-    int n=5;
-    int x=SumOFNatural(n);
+    int x = sumOfNatural(5);
     cout<<x;
 }
+
+
+
+// int SumOFNatural(int n){
+//     return n*(n+1)/2;
+// }
+
+// int main(){
+//     int n=5;
+//     int x=SumOFNatural(n);
+//     cout<<x;
+// }
